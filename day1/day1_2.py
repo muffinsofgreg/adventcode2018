79933bc44fff85e4358fa1c11f0b137ea4e25804
Greg Griffin <griffin.ge@gmail.com>
2018-12-04 13:27:38 -0800

    comment clean up

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -2,9 +2,6 @@ from bs4 import BeautifulSoup
 from scrape import simple_get
 from requests import post
 
-# need to forge the log-in safari cookies into this get request, getting bad
-# content.
-
 # Needed cookie to get 200 response
 cookie = {'session': '53616c7465645f5fa781e68e30ca72571de02155c2b284a09d89968bbb63326bca1d811344eae5316641662f7f3e8230',
           '_gid': 'GA1.2.1346648579.1543826069',
@@ -16,6 +13,8 @@ html = BeautifulSoup(raw_html, 'html.parser')
 stripped_html = html.text.split('\n')
 new_html = []
 
+# This actually achieves what I wanted buy by error. It strips the asci + from the strings,
+# but leaves the "-" when converting to int. 
 try:
     for item in stripped_html:
         new_html.append(int(item))
