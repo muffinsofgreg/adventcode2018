b5ba659c64cbe0566ce1516b4b482b5bac7f4c77
Greg Griffin <griffin.ge@gmail.com>
2018-12-03 19:09:44 -0800

    400 bad request error; need to push cookie with request

--- day1/scrape_input.py
+++ day1/scrape_input.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 from day1_1 import simple_get
-# from htmlmin import minify
 
-raw_html = simple_get('https://adventofcode.com/2018/day/1/input')
-# minify_html = minify(raw_html)
+# need to forge the log-in safari cookies into this get request, getting bad
+# content.
+
+raw_html = simple_get('http://www.google.com')
 html = BeautifulSoup(raw_html, 'html.parser')
 
 for i, li in enumerate(html.select('pre')):
