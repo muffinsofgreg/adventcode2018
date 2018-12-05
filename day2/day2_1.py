3db801e89af5920d78f39ad3ab1341ffe172432d
Greg Griffin <griffin.ge@gmail.com>
2018-12-05 12:06:16 -0800

    day2_1 confirmed correct

--- day2/day2_1.py
+++ day2/day2_1.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from scrape import simple_get
 from requests import post
 from adventcode_sessiondata import cookie
+import collections
 
 raw_html = simple_get('https://adventofcode.com/2018/day/2/input', cookie)
 html = BeautifulSoup(raw_html, 'html.parser')
@@ -14,4 +15,27 @@ stripped_html.pop()
 dupe_dict = {}
 
 for item in stripped_html:
-    pass
+    dupe_dict[item] = collections.Counter(item).most_common(2)
+
+threes = 0
+twos = 0
+
+for item in dupe_dict:
+    if dupe_dict[item][0][1] == 2:
+        twos += 1
+        continue  # if the first duplicate is a 2, then don't count the rest
+
+    if dupe_dict[item][0][1] == 3:
+        threes += 1
+    if dupe_dict[item][1][1] == 2:
+        twos += 1
+
+print(dupe_dict[item])
+print(dupe_dict[item][0])
+print(dupe_dict[item][0][1])
+print()
+print(threes)
+print(twos)
+
+# confirmed, returning the correct answer
+print(threes * twos)
