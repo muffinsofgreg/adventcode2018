33690cc706c75318aa27a7dee2f61a40b163a682
Greg Griffin <griffin.ge@gmail.com>
2018-12-06 00:00:34 -0800

    day2_2 in progress

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -12,30 +12,7 @@ stripped_html = html.text.split('\n')
 # pop off the last element, which is an empty string
 stripped_html.pop()
 
-dupe_dict = {}
+for i in range(len(stripped_html)):
+    for j in range(len())
 
-for item in stripped_html:
-    dupe_dict[item] = collections.Counter(item).most_common(2)
-
-threes = 0
-twos = 0
-
-for item in dupe_dict:
-    if dupe_dict[item][0][1] == 2:
-        twos += 1
-        continue  # if the first duplicate is a 2, then don't count the rest
-
-    if dupe_dict[item][0][1] == 3:
-        threes += 1
-    if dupe_dict[item][1][1] == 2:
-        twos += 1
-
-print(dupe_dict[item])
-print(dupe_dict[item][0])
-print(dupe_dict[item][0][1])
-print()
-print(threes)
-print(twos)
-
-# confirmed, returning the correct answer
-print(threes * twos)
+print(stripped_html)
