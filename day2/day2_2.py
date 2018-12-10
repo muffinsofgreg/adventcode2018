d4548445f58755fdba3b48bb35b4f523ab3dd845
Greg Griffin <griffin.ge@gmail.com>
2018-12-09 19:50:10 -0800

    returns two strings only diff by one char

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -7,12 +7,38 @@ import collections
 raw_html = simple_get('https://adventofcode.com/2018/day/2/input', cookie)
 html = BeautifulSoup(raw_html, 'html.parser')
 
+global stripped_html
 stripped_html = html.text.split('\n')
 
 # pop off the last element, which is an empty string
 stripped_html.pop()
 
-for i in range(len(stripped_html)):
-    for j in range(len())
+# stripped_html = ['abcd', 'abdc', 'bbdc', 'ahct', 'thys', 'usyd']
 
-print(stripped_html)
+
+def find_them():
+    for i in range(len(stripped_html)):
+        for j in range(i + 1, len(stripped_html)):
+            count = 0
+
+            for k in range(len(stripped_html[1])):
+                if stripped_html[i][k] != stripped_html[j][k]:
+                    count += 1
+                    if count > 1:
+                        break
+                else:
+                    pass
+                if k == 25 and count <= 1:
+                    print(stripped_html[i], stripped_html[j])
+                    return (stripped_html[i], stripped_html[j])
+                else:
+                    pass
+
+
+answer = ""
+for a, b in find_them():
+    if a == b:
+        answer += a
+    else:
+        pass
+print(answer)
