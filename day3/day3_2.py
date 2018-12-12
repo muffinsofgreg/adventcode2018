b70954b9843cb4bbd93c19cc1e5d46623ae6d237
Greg Griffin <griffin.ge@gmail.com>
2018-12-12 11:03:59 -0800

    day 3 part 2 complete

--- day3/day3_2.py
+++ day3/day3_2.py
@@ -1,5 +1,7 @@
-# credit to u/TheHungryTurnip
+# credit to u/TheHungryTurnip for Part 1
 # 'https://www.reddit.com/r/adventofcode/comments/a3d6wi/day_3_part_1_troubleshooting/'
+# credit to u/evonfriedland for Part 2 - and for turning me on to NumPy
+# 'https://www.reddit.com/r/adventofcode/comments/a2lesz/2018_day_3_solutions/
 
 import sys
 sys.path.append('..')
@@ -8,6 +10,7 @@ from bs4 import BeautifulSoup  # nopep8
 from scrape import simple_get  # nopep8
 from adventcode_sessiondata import cookie  # nopep8
 import re  # nopep8
+import numpy as np  # nopep8
 
 
 raw_html = simple_get('https://adventofcode.com/2018/day/3/input', cookie)
@@ -21,39 +24,43 @@ stripped_html.pop()
 
 class Claim():
 
-    def __init__(self, x, y, w, h):
+    def __init__(self, n, x, y, w, h):
+        self.n = n
         self.x = x
         self.y = y
         self.w = w
         self.h = h
 
     def __str__(self):
-        return f'({self.x}, {self.y}){self.w}x{self.h}'
+        return f'{self.n}-({self.x}, {self.y}){self.w}x{self.h}'
 
 
 claims = []
 
 for item in stripped_html:
     d = re.split(' |,|: |x', item)
-    c = Claim(int(d[2]), int(d[3]), int(d[4]), int(d[5]))
+    c = Claim(d[0], int(d[2]), int(d[3]), int(d[4]), int(d[5]))
     claims.append(c)
 
-grid = {}
+fabric = np.zeros((1000, 1000))
 
 for item in claims:
-    for h in range(0, item.h):
-        for w in range(0, item.w):
-            coord = f'{item.x + w}x{item.y + h}'
-            if coord in grid:
-                grid[coord] += 1
-            else:
-                grid[coord] = 1
+    n = item.n
+    x = item.x
+    y = item.y
+    dx = item.w
+    dy = item.h
 
-overlaps = 0
+    fabric[x:x + dx, y: y + dy] += 1
 
-# individual cells may = 1, but need to find the square where all cells = 1
-for item in grid:
-    if grid[item] == 1:
-        print(item)
+print(np.sum(fabric > 1))
 
-print(f'overlaps: {overlaps}')
+for item in claims:
+    n = item.n
+    x = item.x
+    y = item.y
+    dx = item.w
+    dy = item.h
+
+    if np.all(fabric[x:x + dx, y:y + dy] == 1):
+        print(n)
